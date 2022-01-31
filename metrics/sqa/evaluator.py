95fbd503e0d1fab890de3198685115857168bcb5
Tianbao Xie <tianbaoxiexxx@gmail.com>
2022-01-31 23:15:57 +0800

    Add interaction acc for SQA dataset

--- metrics/sqa/evaluator.py
+++ metrics/sqa/evaluator.py
@@ -1,30 +1,7 @@
 # encoding=utf8
-from collections import defaultdict
 
 import numpy as np
 from metrics.unified.evaluator import eval_ex_match
-
-#
-# # this function is adapt from tapex
-# def evaluate_example(_predict_str: str, _ground_str: str):
-#     target_delimiter = ', '
-#     _predict_spans = _predict_str.split(target_delimiter)
-#     _ground_spans = _ground_str.split(target_delimiter)
-#     _predict_values = defaultdict(lambda: 0)
-#     _ground_values = defaultdict(lambda: 0)
-#     for span in _predict_spans:
-#         # try:
-#         #     _predict_values[float(span)] += 1
-#         # except ValueError:
-#             _predict_values[span.strip()] += 1
-#     for span in _ground_spans:
-#         # try:
-#         #     _ground_values[float(span)] += 1
-#         # except ValueError:
-#             _ground_values[span.strip()] += 1
-#     _is_correct = _predict_values == _ground_values
-#
-#     return _is_correct
 
 
 class EvaluateTool(object):
@@ -35,22 +12,34 @@ class EvaluateTool(object):
     def evaluate(self, preds, golds, section):
         summary = {}
         all_match = []
+        interaction_match = {}
         pos_match_dic = {"0": [], "1": [], "2": [], "3": []}
 
         for pred, gold_item in zip(preds, golds):
             gold_seq_out = gold_item['seq_out']
             match_or_not = eval_ex_match(pred, gold_seq_out)
+
             # Add the match result to the all set.
             all_match.append(match_or_not)
 
-            # Add the match result to the corresponding position set.
+            # Get the position tag.
             _pos = str(gold_item['position'])
+
+            # Add the match result to the corresponding position set.
             if _pos in pos_match_dic.keys():
                 pos_match_dic[_pos].append(match_or_not)
                 # We only count acc in the top-4 question(pos 0, 1, 2, 3) and all(0,1,2,3,4...)
 
+            # Get the id tag.
+            sid = "{}\t{}".format(gold_item['id'], gold_item['annotator'])
+            if sid not in interaction_match.keys():
+                interaction_match[sid] = []
+            interaction_match[sid].append(match_or_not)
+
         summary["all_acc"] = float(np.mean(all_match))
         for i in pos_match_dic.keys():
             summary["pos_{}_acc".format(i)] = float(np.mean(pos_match_dic[i]))
+        summary["interaction_acc"] = float(
+            np.mean([all(matches) for interaction_id, matches in interaction_match.items()]))
 
         return summary
