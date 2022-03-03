af4d128c4146f30ed6e318a642154f579b3a52b0
Tianbao Xie <tianbaoxiexxx@gmail.com>
2022-03-03 16:19:01 +0800

    Adjust meteor metric for more extensible usage.

--- metrics/dart/evaluator.py
+++ metrics/dart/evaluator.py
@@ -1,6 +1,5 @@
 # encoding=utf8
 
-# from third_party.dart.evaluation.extract_scores_dart import extract_scores_dart
 from datasets import load_metric
 
 
@@ -9,7 +8,11 @@ def postprocess_text(preds, references_s, metric_name):
     preds = [pred.strip() for pred in preds]
     references_s = [[reference.strip() for reference in references] for references in references_s]
 
-    if metric_name in ["metrics/dart/moverscore.py", "meteor", "bleurt"]:
+    if metric_name in ["metrics/dart/moverscore.py", "metrics/dart/meteor.py", "bleurt"]:
+        # select the first reference for moverscore and bleurt metrics follow the practice in DART dataset:
+        # https://github.com/Yale-LILY/dart/blob/4768cf799e58fac412d2d35ec9ee368585152cfc/evaluation/run_eval_on_dart.sh#L24-L29
+        # While for meteor, actually it should be using all references but we misunderstood the official code at first
+        # and we will re-run for a new number and add it to appendix in the next version -- 3.3.2022
         references_s = [references[0] for references in references_s]
     elif metric_name in ["sacrebleu"]:
         # since hf sacrebleu only support references with same length, we have to pad them into the same length
@@ -37,12 +40,16 @@ class EvaluateTool(object):
         if section in ['train', 'dev']:
             metric_list = ["sacrebleu"]
         elif section == 'test':
-            metric_list = ["sacrebleu", "meteor", "metrics/dart/ter.py",
-            # "metrics/dart/moverscore.py"
-            # # moverscore strictly depends on hf==3.1.0 which conflicts our repo dependency.
-                                                  "bertscore", "bleurt"]
+            metric_list = ["sacrebleu", "metrics/dart/meteor.py", "metrics/dart/ter.py",
+                           # Huggingface has their implementation in Nov 2021:
+                           # ter https://github.com/huggingface/datasets/tree/master/metrics/ter,
+                           # it is equal in function to ours.
+                           # "metrics/dart/moverscore.py"
+                           # we didn't use moverscore since it strictly depends on hf==3.1.0
+                           # which conflicts our repo dependency.
+                           "bertscore", "bleurt"]
 
-        # TODO: parent is not handy in Python, we will leave it in the final eval.
+        # TODO: Adopt PARENT metric from ToTTo.
         for metric_name in metric_list:
             metric = load_metric(metric_name)
             processed_preds, processed_golds = postprocess_text(preds, references_s, metric_name)
@@ -58,22 +65,15 @@ class EvaluateTool(object):
                 res = metric.compute(predictions=processed_preds, references=processed_golds)
                 if metric_name == "sacrebleu":
                     summary[metric_name] = res["score"] * 0.01
+                    print(metric_name, res)
                 elif metric_name == "bleurt":
                     summary["bleurt"] = round(1.0 * sum(res["scores"]) / len(res["scores"]), 2)
-                elif metric_name == "metrics/dart/moverscore.py":
-                    summary[metric_name] = res["moverscore"]
+                elif metric_name == "metrics/dart/meteor.py":
+                    summary[metric_name] = res["meteor"]
                 elif metric_name == "metrics/dart/ter.py":
                     summary[metric_name] = res["ter"]
+                elif metric_name == "metrics/dart/moverscore.py":
+                    summary[metric_name] = res["moverscore"]
                 else:
                     summary[metric_name] = res[metric_name]
         return summary
-
-if __name__ == '__main__':
-    import json
-
-    with open("predictions_predict.json") as f:
-        test_data = json.load(f)
-    preds = [item['prediction'] for item in test_data]
-    evaluator = EvaluateTool(args=None)
-    score = evaluator.evaluate(preds, test_data, section="test")
-    print(score)
